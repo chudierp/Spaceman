b484bed7ee11604cd83b57df9e7343186deeaf41
Chudier <chudierp@gmail.com>
2019-09-10 15:54:15 -0700

    completed TODO for is_guess_in_word

--- spaceman.py
+++ spaceman.py
@@ -26,10 +26,11 @@ def is_word_guessed(secret_word, letters_guessed):
     '''
     # TODO: Loop through the letters in the secret_word and check if a letter is not in letters_guessed
     for letters in secret_word:
-        if letter is not in letters_guessed:
+        if letters not in letters_guessed:
             return False
-        else return True    
-    pass 
+        else: 
+            return True    
+    #pass 
 
 def get_guessed_word(secret_word, letters_guessed):
     '''
@@ -64,7 +65,10 @@ def is_guess_in_word(guess, secret_word):
         bool: True if the guess is in the secret_word, False otherwise
     '''
     #TODO: check if the letter guess is in the secret word
-
+    if guess in secret_word:
+        return True 
+    else: 
+        return False     
     pass
 
 
@@ -80,6 +84,9 @@ def spaceman(secret_word):
 
     #TODO: show the player information about the game according to the project spec
 
+    
+    
+    
     #TODO: Ask the player to guess one letter per round and check that it is only one letter
 
     #TODO: Check if the guessed letter is in the secret or not and give the player feedback
